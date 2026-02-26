4cfb5fba4836ea304074be105399cbdb70d6642a
Mag-C <magalie.cyr06@gmail.com>
2026-02-26 16:52:52 -0500

    Poutine.js Completed

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,9 +1,32 @@
 export default class Poutine {
-  constructor() {
+  constructor(element) {
+    this.element = element;
+    this.types = this.element.querySelectorAll('.js-button-poutine');
+    this.selectedType = '';
     this.init();
   }
 
   init() {
-    console.log('Main init working');
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+    }
+  }
+
+  selectType(e) {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.classList.remove('is-active');
+      e.currentTarget.classList.add('is-active');
+      this.selectedType = e.currentTarget.innerText;
+    }
+    console.log(this.selectedType);
+    this.updatePhoto();
+  }
+
+  updatePhoto() {
+    const image = this.element.querySelector('img');
+    image.classList.add('is-active');
+    image.src = `assets/images/${this.selectedType}.png`;
   }
 }
